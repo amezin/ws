a5dbaf3e4d3004f587c5bc8c658b38da42fee382
Aleksandr Mezin <mezin.alexander@gmail.com>
2017-07-13 19:16:05 +0600

    Better tests: a server behind NAT doesn't respond

--- ws_nat/test_functional.py
+++ ws_nat/test_functional.py
@@ -22,6 +22,16 @@ async def test_all(test_server):
         assert received == [message]
 
 
+class NattedReceiver(receiver.Receiver):
+    async def handle_server_connection(self, request):
+        await asyncio.Future()
+
+
+class NattedSender(sender.Sender):
+    async def handle_server_connection(self, request):
+        await asyncio.Future()
+
+
 async def test_receiver_nat(test_server):
     intermediate_app = intermediate_server.IntermediateServerApp()
     server = await test_server(intermediate_app)
@@ -30,12 +40,12 @@ async def test_receiver_nat(test_server):
         message = "TEST MESSAGE " + str(i)
 
         received = []
-        receiver_obj = receiver.Receiver(server.make_url('/'), output=received.append)
+        receiver_obj = NattedReceiver(server.make_url('/'), output=received.append)
 
         sender_obj = sender.Sender(server.make_url('/'))
         sender_obj.data.put_nowait(message)
 
-        await asyncio.gather(asyncio.ensure_future(receiver_obj.run_client()),
+        await asyncio.gather(asyncio.ensure_future(receiver_obj.run_client_server()),
                              asyncio.ensure_future(sender_obj.run_client_server()))
 
         assert received == [message]
@@ -51,10 +61,10 @@ async def test_sender_nat(test_server):
         received = []
         receiver_obj = receiver.Receiver(server.make_url('/'), output=received.append)
 
-        sender_obj = sender.Sender(server.make_url('/'))
+        sender_obj = NattedSender(server.make_url('/'))
         sender_obj.data.put_nowait(message)
 
         await asyncio.gather(asyncio.ensure_future(receiver_obj.run_client_server()),
-                             asyncio.ensure_future(sender_obj.run_client()))
+                             asyncio.ensure_future(sender_obj.run_client_server()))
 
         assert received == [message]
